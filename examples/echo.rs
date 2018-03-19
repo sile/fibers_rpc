1c419597b50c2b1e7b236cb5d141cebcb1f472d8
Takeru Ohta <phjgt308@gmail.com>
2018-03-19 23:55:35 +0900

    Add `bench` subcommand

--- examples/echo.rs
+++ examples/echo.rs
@@ -8,14 +8,14 @@ extern crate trackable;
 
 use std::io::{self, Read, Write};
 use std::net::ToSocketAddrs;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use clap::{App, Arg, SubCommand};
-use fibers::{Executor, InPlaceExecutor, Spawn};
+use fibers::{Executor, Spawn, ThreadPoolExecutor};
 use fibers_rpc::{Call, ProcedureId};
-use fibers_rpc::client::ClientServiceBuilder;
+use fibers_rpc::client::{ClientServiceBuilder, ClientServiceHandle};
 use fibers_rpc::codec::BytesEncoder;
 use fibers_rpc::server::{HandleCall, Reply, ServerBuilder};
-use futures::Future;
+use futures::{Async, Future, Poll};
 use sloggers::Build;
 use sloggers::terminal::TerminalLoggerBuilder;
 use sloggers::types::Severity;
@@ -73,6 +73,23 @@ fn main() {
                         .default_value("1"),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("bench")
+                .arg(
+                    Arg::with_name("CONCURRENCY")
+                        .long("concurrency")
+                        .short("c")
+                        .takes_value(true)
+                        .default_value("256"),
+                )
+                .arg(
+                    Arg::with_name("REQUESTS")
+                        .long("requests")
+                        .short("n")
+                        .takes_value(true)
+                        .default_value("1000"),
+                ),
+        )
         .get_matches();
 
     let addr = track_try_unwrap!(
@@ -89,7 +106,7 @@ fn main() {
     let log_level: Severity = track_try_unwrap!(matches.value_of("LOG_LEVEL").unwrap().parse());
     let logger = track_try_unwrap!(TerminalLoggerBuilder::new().level(log_level).build());
 
-    let mut executor = track_try_unwrap!(InPlaceExecutor::new().map_err(Failure::from_error));
+    let mut executor = track_try_unwrap!(ThreadPoolExecutor::new().map_err(Failure::from_error));
 
     if let Some(_matches) = matches.subcommand_matches("server") {
         let server = ServerBuilder::new(addr)
@@ -128,8 +145,95 @@ fn main() {
             let response = track_try_unwrap!(result);
             let _ = std::io::stdout().write(&response);
         }
+    } else if let Some(matches) = matches.subcommand_matches("bench") {
+        let concurrency: usize = track_try_unwrap!(track_any_err!(
+            matches.value_of("CONCURRENCY").unwrap().parse()
+        ));
+        let requests: usize = track_try_unwrap!(track_any_err!(
+            matches.value_of("REQUESTS").unwrap().parse()
+        ));
+
+        let service = ClientServiceBuilder::new()
+            .logger(logger)
+            .finish(executor.handle());
+        let client_service = service.handle();
+        executor.spawn(service.map_err(|e| panic!("{}", e)));
+
+        let mut buf = Vec::new();
+        track_try_unwrap!(
+            io::stdin()
+                .read_to_end(&mut buf)
+                .map_err(Failure::from_error)
+        );
+
+        let (finish_tx, finish_rx) = std::sync::mpsc::channel();
+        let start_time = Instant::now();
+        for i in 0..concurrency {
+            let finish_tx = finish_tx.clone();
+            let future = Bench {
+                i,
+                step: concurrency,
+                n: requests,
+                input: buf.clone(),
+                service: client_service.clone(),
+                server: addr,
+                future: None,
+            };
+            executor.spawn(future.then(move |result| {
+                let _ = finish_tx.send(());
+                if let Err(e) = result {
+                    println!("{}", e);
+                }
+                Ok(())
+            }));
+        }
+        std::thread::spawn(move || {
+            track_try_unwrap!(track_any_err!(executor.run()));
+        });
+        for _ in 0..concurrency {
+            track_try_unwrap!(track_any_err!(finish_rx.recv()));
+        }
+        let elapsed = start_time.elapsed();
+        let seconds = elapsed.as_secs() as f64 + (elapsed.subsec_nanos() as f64 / 1_000_000_000.0);
+        println!("# ELAPSED: {}", seconds);
+        println!("# RPS: {}", requests as f64 / seconds);
     } else {
         println!("{}", matches.usage());
         std::process::exit(1);
+    }
+}
+
+struct Bench {
+    i: usize,
+    step: usize,
+    n: usize,
+    service: ClientServiceHandle,
+    server: std::net::SocketAddr,
+    input: Vec<u8>,
+    future: Option<fibers_rpc::client::Response<Vec<u8>>>,
+}
+impl Future for Bench {
+    type Item = ();
+    type Error = fibers_rpc::Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        loop {
+            if self.future.is_none() {
+                if self.i >= self.n {
+                    return Ok(Async::Ready(()));
+                }
+
+                let client = EchoRpc::client(&self.service);
+                let future = client.call(self.server, self.input.clone());
+                self.future = Some(future);
+                self.i += self.step;
+            }
+            if let Async::Ready(Some(_)) = track!(self.future.poll())? {
+                self.future = None;
+                continue;
+            }
+            break;
+        }
+        Ok(Async::NotReady)
     }
 }
